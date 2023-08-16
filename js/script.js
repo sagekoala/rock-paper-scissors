8477c807b11b946a04c196aa28f39b7bb8177ce8
sagekoala <brandondukescodes@gmail.com>
2023-08-16 18:19:38 -0500

    Abstraction code into separate functions
    Abstracted functional code into the playTournament() and
    displayResults() functions.

--- js/script.js
+++ js/script.js
@@ -11,71 +11,64 @@ function getComputerChoice() {
     return choices[rand_num];
 }
 
-// Write playRound function that takes in userSelection and a callback
-// to compSelection and returns statement listed winner and loser
+// Write playRound function that takes userChoice and compChoice and returns winner
 function playRound(userChoice, getComputerChoice) {
 
     // Exectute function and store compChoice
     const compChoice = getComputerChoice();
-    let winner = "";
-    let loser = "";
     
-    // Compare comp and user choices
+    // Compare comp and user choices, return results
     if (compChoice === userChoice) {
-        console.log("TIE");
-        return ["tie"];
+        return ["tie", compChoice, userChoice];
     } else if (compChoice === "rock" && userChoice === "scissors") {
-        console.log(`compChoice wins! ${compChoice} beats ${userChoice}`);
-        winner = "computer";
-        loser = "user";
-        return [winner, loser];
+        return ["computer", "user", compChoice, userChoice];
     } else if (compChoice === "rock" && userChoice === "paper") {
-        console.log(`userChoice wins! ${userChoice} beats ${compChoice}`);
-        winner = "user";
-        loser = "computer";
-        return [winner, loser];
+        return ["user", "computer", userChoice, compChoice];
     } else if (compChoice === "paper" && userChoice === "rock") {
-        console.log(`compChoice wins! ${compChoice} beats ${userChoice}`);
-        winner = "computer";
-        loser = "user";
-        return [winner, loser];
+        return ["computer", "user", compChoice, userChoice];
     } else if (compChoice === "paper" && userChoice === "scissors") {
-        console.log(`userChoice wins! ${userChoice} beats ${compChoice}`);
-        winner = "user";
-        loser = "computer";
-        return [winner, loser];
+        return ["user", "computer", userChoice, compChoice];
     } else if (compChoice === "scissors" && userChoice === "rock") {
-        console.log(`userChoice wins! ${userChoice} beats ${compChoice}`); 
-        winner = "user";
-        loser = "computer";
-        return [winner, loser];
+        return ["user", "computer", userChoice, compChoice];
     } else if (compChoice === "scissors" && userChoice === "paper") {
-        console.log(`compChoice wins! ${compChoice} beats ${userChoice}`);
-        winner = "computer";
-        loser = "user";
-        return [winner, loser];
+        return ["computer", "user", compChoice, userChoice];
     }
 }
 
-// Write loop that calls playRound function n times
-// prints winner and keeps track of score
-
-// Initialize score variables
-let userScore = 0;
-let compScore = 0;
-let tally;
-for (let i = 0; i < 5; i ++) {
-    
-    // Prompt user for a selection and store value in a variable
-    const userChoice = prompt("Choose a value rock, paper or scissors: ");  
-    tally = playRound(userChoice, getComputerChoice);
-
-    if (tally[0] === "user") {
-        userScore++;
-    } else if (tally[0] === "computer") {
-        compScore++;
-    } 
-
-    console.log(`User: ${userScore}, Computer: ${compScore}`);
+// Display results based on results returned by playRound()
+function displayResults(results) {
+    if (results[0] === "user") {
+        console.log(`userChoice wins! ${results[2]} beats ${results[3]}`); 
+    } else if (results[0] === "computer") {
+        console.log(`compChoice wins! ${results[2]} beats ${results[3]}`);
+    } else {
+        console.log(`Tie! Computer Choice: ${results[1]} User Choice: ${results[2]}`);
+    }
 }
 
+// Plays tournament of n rounds
+function playTournament(n) {
+
+    // Initialize results variable to serve as string array
+    let results;
+    
+    // Initialize score variables
+    let userScore = 0;
+    let compScore = 0;
+
+    for (let i = 0; i < n; i ++) {
+        // Prompt user for a selection and store value in a variable
+        const userChoice = prompt("Choose a value rock, paper or scissors: ");  
+        results = playRound(userChoice, getComputerChoice);
+        displayResults(results);
+        if (results[0] === "user") {
+            userScore++;
+        } else if (results[0] === "computer") {
+            compScore++;
+        }
+        console.log(`User: ${userScore} - Computer: ${compScore}`);
+    }
+}
+
+// Plays tournament of n rounds
+playTournament(5);
