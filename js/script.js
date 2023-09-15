3a5620599b962f8706438e0a6734b94ec7eef2d1
sagekoala <brandondukescodes@gmail.com>
2023-09-15 18:18:25 -0500

    Add new functions to the program
    Functions:
    eventHandler, playRound, getRoundWinner, endMatch, updateScore,
    displayWinner
    
    Created named function eventHandler to pass the innerText value
    of the button that the user selects to the playRound function.
    Additionally, created the named function eventHandler to be passed to
    addEventListener as a listener and then later passed to
    removeEventListener once a given opponent reached a score of 5.
    playRound compares playerSelection against computerSelection, calls
    getRoundWinner, passing in each selection as parameters and stores the
    return value. Next updateScore is called, passing in roundWinner as
    a parameter. The updateScore function updates the scores dictionary
    and checks if either opponent has reached a score of 5 at a given
    moment. Once an opponent reaches 5, the endMatch function is called
    to removeEventListener on each of the buttons and then the displayWinner
    function is called.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,16 @@
 // Storing array of button element nodes
 const buttons = document.querySelectorAll('button');
 
-// Calling function, handling button event that was clicked
-// i.e. user choice
-playRound(function(result) {
-    
-    const playerSelection = result.toLowerCase();
-    console.log(`Player selection : ${playerSelection}`);
-    const computerSelection = getComputerChoice().toLowerCase();
-    console.log(`Computer selection : ${computerSelection}`);
-    const roundWinner = getRoundWinner(playerSelection, computerSelection);
-    console.log(`Round winner ${roundWinner}`);
+// Declare scores dictionary
+let scores = {
+    "Player": 0,
+    "Computer": 0
+};
 
-});
-
-// Function that will return value from eventListener i.e. event handler
-function playRound(callback) {
-    buttons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            callback(e.srcElement.innerText);
-        });
-    });
-}
 
 function getComputerChoice() {
+
+    // Return random choice from choices array
     const choices = ['rock', 'paper', 'scissors'];
     const rand_num = Math.floor(Math.random() * 3);
 
@@ -32,7 +19,7 @@ function getComputerChoice() {
 
 function getRoundWinner(playerSelection, computerSelection) {
 
-    // Compare computer and player choices, return results
+    // Compare computer and player choices, return result
     if (computerSelection === playerSelection) {
         return 'Tie';
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
@@ -49,3 +36,108 @@ function getRoundWinner(playerSelection, computerSelection) {
         return 'Computer';
     }
 }
+
+buttons.forEach((button) => {
+    button.addEventListener('click', eventHandler);
+});
+
+// Pass player selection to playRound() function
+function eventHandler(e) {
+    playRound(e.srcElement.innerText);
+}
+
+function endMatch() {
+    buttons.forEach((button) => {
+        button.removeEventListener();
+    });
+}
+
+
+function playRound(result) {
+    
+    const playerSelection = result.toLowerCase();
+    const computerSelection = getComputerChoice().toLowerCase();
+    const roundWinner = getRoundWinner(playerSelection, computerSelection);
+    console.log(`P ${playerSelection} | C${computerSelection}`);
+    updateScore(roundWinner);
+
+}
+
+function endMatch() {
+    buttons.forEach((button) => {
+        button.removeEventListener('click', eventHandler);
+    });
+}
+
+function updateScore(roundWinner) {
+    // Update player scores except for tie
+    if (`${roundWinner}` in scores) {
+        scores[`${roundWinner}`] += 1;
+    }
+
+    // Once an opponent reaches 5 points, end match
+    if (scores["Player"] === 5 || scores["Computer"] === 5) {
+        endMatch();
+        displayWinner(); // function that displays text div with winner's name
+    }
+
+    console.log(scores);
+}
+
+function displayWinner() {
+
+    // Loop through dict, store key with value of 5
+    for (let key in scores) {
+        if (scores[key] === 5) {
+            console.log(`name of person with 5 ${key}`);
+            console.log(scores);
+        }
+    }
+}
+
+// In order to remove event listener 
+// you have to have used a named function
+// for your eventHandler function
+// that way you can "dereference it" with
+// a removeEventListener() function that
+// calls the exact same eventHandler function
+
+/*
+// Calling function, handling button event that was clicked
+// i.e. user choice
+playRound(function(result) {
+
+    const playerSelection = result.toLowerCase();
+    const computerSelection = getComputerChoice().toLowerCase();
+    let roundWinner = getRoundWinner(playerSelection, computerSelection);
+
+    if (roundWinner === "Player") {
+        scores["playerScore"] += 1;
+    } else if (roundWinner === "Computer") {
+        scores["computerScore"] += 1;
+    }
+
+    console.log(`Player selection : ${playerSelection}`);
+    console.log(`Computer selection : ${computerSelection}`);
+    console.log(`Round winner ${roundWinner}`);
+    console.log(scores);
+
+    if (scores["playerScore"] > 1) {
+        console.log("RUNNING END MATCH");
+        endMatch();
+    }
+
+    console.log("ANOTHER ROUND");
+
+});
+
+// Function that will return value from eventListener i.e. event handler
+function playRound(callback) {
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            callback(e.srcElement.innerText);
+        });
+    });
+}
+
+*/
